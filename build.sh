186ce5cbdee401e0dfc884d01f7ea161d91035cb
feiyangguo <feiyang.guo@iglooinsure.com>
2021-08-17 13:43:35 +0800

    k8s cronkjob demo

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ source /etc/profile
 VERSION=$(git describe --tags 2>/dev/null)
 COMMIT=$(git rev-parse --short HEAD)
 TIME=$(date +%FT%T)
+MAIN_PATH=$1
 if [ -z $VERSION ]; then
     VERSION=$COMMIT
 fi
 
 VerPkg="myGo"
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X ${VerPkg}.Version=$VERSION -X ${VerPkg}.GitCommit=$COMMIT -X ${VerPkg}.BuildTime=${TIME}"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -o myGo -ldflags "-X ${VerPkg}.Version=$VERSION -X ${VerPkg}.GitCommit=$COMMIT -X ${VerPkg}.BuildTime=${TIME}" "$MAIN_PATH"
---
+++ build/docker_build.sh
@@ -0,0 +1,1 @@
+docker build -t crontest:v1 .
